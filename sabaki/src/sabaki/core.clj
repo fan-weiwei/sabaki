d91a6990d9484f3b821b18d0cd527a60f6000bef
范薇薇 <fanweiweiniuyue@gmail.com>
2016-12-19 22:20:20 -0500

    Simplified code

--- sabaki/src/sabaki/core.clj
+++ sabaki/src/sabaki/core.clj
@@ -6,29 +6,30 @@
   (require [sabaki.encoding :refer :all])
   (require [sabaki.ops :refer :all]))
 
-(defn -main [& args]
 
-  (let [begin (System/currentTimeMillis)]
 
-  (defn process [cipher-text channel] (go
-           (let [result (sleaze-single-xor cipher-text)]
-             (if (> (count result) 0) (>! channel result)))
+(def lines (doall
+             (->> (slurp "single-cipher")
+                  (clojure.string/split-lines))))
 
-  ))
+(defn sleaze-single-cipher []
 
-  (def lines (doall
-               (->> (slurp "single-cipher")
-                    (clojure.string/split-lines))))
-
-  (def channels (for [line lines] (chan)))
-
-  (let [ chans (partition-all 2 (interleave lines (for [line lines] (chan))))
-         chans-only (mapv second chans) ]
-
-    (doseq [pair chans] (apply process pair))
-    (let [[v p] (alts!! chans-only)]
-      (println "Result: " v))
-      (println "Waited (ms) :" (- (System/currentTimeMillis) begin))
+   (defn process [cipher-text channel]
+      (go (let [result (sleaze-single-xor cipher-text)]
+          (if (> (count result) 0) (>! channel result))))
    )
+
+   (let [ chans (partition-all 2 (interleave lines (for [line lines] (chan))))
+          chans-only (mapv second chans) ]
+
+      (doseq [pair chans] (apply process pair))
+      (let [[v p] (alts!! chans-only)] (println "Result: " v))
+   )
+
+)
+
+(defn -main [& args]
+  (time
+    (sleaze-single-cipher)
   )
 )
