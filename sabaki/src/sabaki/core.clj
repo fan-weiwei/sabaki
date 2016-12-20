210f375093ad581bb71f6b9258951e3d0647b3fe
范薇薇 <fanweiweiniuyue@gmail.com>
2016-12-19 22:14:33 -0500

    Reduced time to 6 seconds from 20 seconds

--- sabaki/src/sabaki/core.clj
+++ sabaki/src/sabaki/core.clj
@@ -1,24 +1,34 @@
 (ns sabaki.core
+  (require [clojure.core.async
+            :as a
+            :refer [>! <! >!! <!! go chan buffer close! thread
+                    alts! alts!! timeout]])
   (require [sabaki.encoding :refer :all])
   (require [sabaki.ops :refer :all]))
 
-
-(defn sleaze-single-cipher []
-  (def result
-    (->> (slurp "single-cipher")
-         (clojure.string/split-lines)
-         (mapcat sleaze-single-xor)
-         (filter #(not= (count %) 0))
-         )
-    )
-
-  (println result)
-)
-
 (defn -main [& args]
 
-  (time (sleaze-single-cipher))
+  (let [begin (System/currentTimeMillis)]
 
+  (defn process [cipher-text channel] (go
+           (let [result (sleaze-single-xor cipher-text)]
+             (if (> (count result) 0) (>! channel result)))
+
+  ))
+
+  (def lines (doall
+               (->> (slurp "single-cipher")
+                    (clojure.string/split-lines))))
+
+  (def channels (for [line lines] (chan)))
+
+  (let [ chans (partition-all 2 (interleave lines (for [line lines] (chan))))
+         chans-only (mapv second chans) ]
+
+    (doseq [pair chans] (apply process pair))
+    (let [[v p] (alts!! chans-only)]
+      (println "Result: " v))
+      (println "Waited (ms) :" (- (System/currentTimeMillis) begin))
+   )
+  )
 )
-
-
